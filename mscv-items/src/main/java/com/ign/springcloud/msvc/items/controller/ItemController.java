adfc1167a9582ff8203ed57539d4ab4119d830c4
ignjs <ignacio.jara.s@gmail.com>
2025-01-13 23:25:49 -0300

    Agregar configuración de Circuit Breaker y Time Limiter en el microservicio de ítems, incluyendo métodos de recuperación y detalles de implementación en el controlador.

--- mscv-items/src/main/java/com/ign/springcloud/msvc/items/controller/ItemController.java
+++ mscv-items/src/main/java/com/ign/springcloud/msvc/items/controller/ItemController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ign.springcloud.msvc.items.model.Item;
 import com.ign.springcloud.msvc.items.model.Product;
 import com.ign.springcloud.msvc.items.service.ItemService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 public class ItemController {
@@ -42,6 +46,18 @@ public class ItemController {
 		return service.findAll();
 	}
 
+	/**
+	 * Handles GET requests to retrieve the details of an item by its ID.
+	 * 
+	 * @param id the ID of the item to retrieve
+	 * @return a ResponseEntity containing the item details if found, or a 404 NOT
+	 *         FOUND status with an error message if the item does not exist
+	 * 
+	 *         The method uses a circuit breaker to handle potential failures when
+	 *         calling the service to find the item by ID.
+	 *         If the service call fails, a default item is returned with predefined
+	 *         values.
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<?> details(@PathVariable Long id) {
 		/* Optional<Item> itemOptional = service.findAById(id); */
@@ -61,5 +77,69 @@ public class ItemController {
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.body(Collections.singletonMap("message", "No existe el producto"));
+	}
+
+	/**
+	 * Retrieves the details of an item by its ID.
+	 * 
+	 * This method uses a CircuitBreaker named "items" to handle potential failures.
+	 * If the item is found, it returns the item details with an HTTP 200 status.
+	 * If the item is not found, it returns an HTTP 404 status with a message
+	 * indicating
+	 * that the product does not exist.
+	 * 
+	 * @param id the ID of the item to retrieve
+	 * @return a ResponseEntity containing the item details or an error message
+	 */
+	@CircuitBreaker(name = "items", fallbackMethod = "getFallbackProduct")
+	@GetMapping("/details/{id}")
+	public ResponseEntity<?> details2(@PathVariable Long id) {
+		/* Optional<Item> itemOptional = service.findAById(id); */
+		Optional<Item> itemOptional = service.findAById(id);
+		if (itemOptional.isPresent()) {
+			return ResponseEntity.ok(itemOptional.get());
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(Collections.singletonMap("message", "No existe el producto"));
+	}
+
+	/**
+	 * Fallback method to handle exceptions and provide a default response.
+	 *
+	 * @param e the exception that triggered the fallback
+	 * @return a ResponseEntity containing a default Item with a predefined Product
+	 */
+	public ResponseEntity<?> getFallbackProduct(Throwable e) {
+		log.error(e.getMessage());
+		Product product = new Product();
+		product.setCreateAt(LocalDate.now());
+		product.setId(1L);
+		product.setName("Amazon Fire TV Stick");
+		product.setPrice(39.99);
+		return ResponseEntity.ok(new Item(product, 5));
+	}
+
+	/**
+	 * Retrieves the details of an item asynchronously by its ID.
+	 * This method is protected by a TimeLimiter named "items".
+	 *
+	 * @param id the ID of the item to retrieve
+	 * @return a CompletableFuture containing the ResponseEntity with the item
+	 *         details if found,
+	 *         or a ResponseEntity with a not found status and a message if the item
+	 *         does not exist
+	 */
+	@TimeLimiter(name = "items")
+	@GetMapping("/details3/{id}")
+	public CompletableFuture<?> details3(@PathVariable Long id) {
+		/* Optional<Item> itemOptional = service.findAById(id); */
+		return CompletableFuture.supplyAsync(() -> {
+			Optional<Item> itemOptional = service.findAById(id);
+			if (itemOptional.isPresent()) {
+				return ResponseEntity.ok(itemOptional.get());
+			}
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(Collections.singletonMap("message", "No existe el producto"));
+		});
 	}
 }
--- msvc-product/src/main/java/com/ign/springcloud/msvc/products/controller/ProductController.java
+++ msvc-product/src/main/java/com/ign/springcloud/msvc/products/controller/ProductController.java
@@ -33,7 +33,7 @@ public class ProductController {
 		}
 
 		if (id.equals(7L)) {
-			TimeUnit.SECONDS.sleep(5L);
+			TimeUnit.SECONDS.sleep(3L);
 		}
 
 		Optional<Product> productOptional = service.findById(id);
