5f8ebad8db12ecf7ce1770167ec17237e329290e
ignjs <ignacio.jara.s@gmail.com>
2025-01-16 20:40:07 -0300

    Configuración inicial del Gateway Server
    - Configuración básica del servidor en application.properties
    - Documentación del proyecto en el README.md
    - Configuración de enrutamiento para los microservicios
    - Instrucciones de instalación y ejecución

---
+++ src/main/java/com/ign/springcloud/msvc/gateway/MsvcgatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.ign.springcloud.msvc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcgatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcgatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ign/springcloud/msvc/gateway/MsvcgatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ign.springcloud.msvc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
