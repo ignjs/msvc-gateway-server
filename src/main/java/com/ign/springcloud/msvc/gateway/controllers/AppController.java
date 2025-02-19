9c3fa8f38fb65b2fc303d3cacd40c2bf691509ed
ignjs <ignacio.jara.s@gmail.com>
2025-02-18 23:45:04 -0300

    Add OAuth2 security configuration and authorization endpoints

---
+++ src/main/java/com/ign/springcloud/msvc/gateway/controllers/AppController.java
@@ -0,0 +1,30 @@
+package com.ign.springcloud.msvc.gateway.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class AppController {
+    @GetMapping("/authorized")
+    public Map<String, String> authorize(@RequestParam String code) {
+        Map<String, String> map = new HashMap<>();
+        map.put("code", code);
+        return map;
+    }
+
+    @PostMapping("/logout")
+    public Map<String, String> logout() {
+        //TODO: process POST request        
+        return Collections.singletonMap("logout", "success");
+    }
+    
+    
+
+}
