0c59cde01e39f5bf598d06d93478c2bb8ebf378a
ignjs <ignacio.jara.s@gmail.com>
2025-02-26 21:04:32 -0300

    Enhance OAuth2 resource server configuration with custom JWT authentication converter

--- src/main/java/com/ign/springcloud/msvc/gateway/security/SecurityConfig.java
+++ src/main/java/com/ign/springcloud/msvc/gateway/security/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.ign.springcloud.msvc.gateway.security;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.lang.Nullable;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class SecurityConfig {
@@ -30,26 +38,45 @@ public class SecurityConfig {
         }).cors(csrf -> csrf.disable())
                 .oauth2Login(withDefaults())
                 .oauth2Client(withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, Mono<AbstractAuthenticationToken>>() {
+
+                            @Override
+                            @Nullable
+                            public Mono<AbstractAuthenticationToken> convert(Jwt source) {
+                                Collection<String> roles = source.getClaimAsStringList("roles");
+                                Collection<GrantedAuthority> authorities = roles.stream()
+                                        .map(SimpleGrantedAuthority::new)
+                                        .collect(Collectors.toList());
+                                return Mono.just(new JwtAuthenticationToken(source, authorities));
+                            }
+                        })))
                 .build();
+
     }
 
-   /*  @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((authz) -> {
-            authz
-                    .requestMatchers("/authorized", "/logout").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/products", "/api/items", "/api/users").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/products/{id}", "/api/items/{id}", "/api/users/{id}")
-                    .hasAnyRole("ADMIN", "USER")
-                    .requestMatchers("/api/products/**", "/api/items/**", "/api/users/**").hasRole("ADMIN")
-                    .anyRequest().authenticated();
-        })
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(csrf -> csrf.disable())
-                .oauth2Login(login -> login.loginPage("/oauth2/authorization/client-app"))
-                .oauth2Client(withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
-                .build();
-    } */
+    /*
+     * @Bean
+     * SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     * return http.authorizeHttpRequests((authz) -> {
+     * authz
+     * .requestMatchers("/authorized", "/logout").permitAll()
+     * .requestMatchers(HttpMethod.GET, "/api/products", "/api/items",
+     * "/api/users").permitAll()
+     * .requestMatchers(HttpMethod.GET, "/api/products/{id}", "/api/items/{id}",
+     * "/api/users/{id}")
+     * .hasAnyRole("ADMIN", "USER")
+     * .requestMatchers("/api/products/**", "/api/items/**",
+     * "/api/users/**").hasRole("ADMIN")
+     * .anyRequest().authenticated();
+     * })
+     * .sessionManagement(session ->
+     * session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+     * .csrf(csrf -> csrf.disable())
+     * .oauth2Login(login -> login.loginPage("/oauth2/authorization/client-app"))
+     * .oauth2Client(withDefaults())
+     * .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+     * .build();
+     * }
+     */
 }
